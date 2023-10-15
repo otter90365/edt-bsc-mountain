2eac39c28b802c85aa7344e2aebccf733ce3cbd4
starbit_phoebe <phoebe.cheng@star-bit.io>
2023-10-15 22:42:19 +0800

    [edit] Defi 內頁首頁 內文 移除不用的東西

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,16 +23,13 @@
 import borderWrapCard from '@/components/borderWrapCard.vue'
 import titleBlock from '@/components/titleBlock.vue'
 import imgBtn from '@/components/imgBtn.vue'
-import Game from "@/plugins/game.js";
 import Defi from "@/plugins/defi.js";
 export default {
   name: "Home",
   data(){
     return {
-      gameContract: null,
       defiContract: null,
-      totalAmount: '--',
-      roundAmount: '--'
+      totalAmount: '--'
     }
   },
   components:{
@@ -42,14 +39,6 @@ export default {
     imgBtn
   },
   methods:{
-    toGame(){
-      if (this.$store.state.account){
-        // this.$router.push({name: 'Game'})
-        this.$toasted.error('因鏈上節點故障，彩金池維修中')
-      }else{
-        this.$toasted.error(this.$t('loginFirst'))
-      }
-    },
     toSwap(){
       if (this.$store.state.account){
         // this.$router.push({name: 'Egt-swap', params: {lang: this.$store.state.locale, token: this.$route.params.token}})
@@ -65,16 +54,6 @@ export default {
         this.totalAmount = 0
         console.log('error', error)
       }
-    },
-    async getRoundAmount(){
-      try{
-        let round = await this.gameContract.getCurrRound()
-        let detail = await this.gameContract.getRoundDetails(round)
-        this.roundAmount = detail.roundamount
-      }catch(error){
-        this.roundAmount = 0
-        console.log('error', error)
-      }
     }
   },
   created(){
@@ -83,23 +62,13 @@ export default {
     }
   },
   async mounted(){
-    // console.log('==========index==========')
-    this.gameContract = await new Game()
     this.defiContract = await new Defi()
     await this.getTotalAmount()
-    await this.getRoundAmount()
   }
 }
 </script>
 <style lang="scss">
 .index{
-  .bsc-egt-block{
-    width: 100%;
-    border: 2px solid #0abbb5;
-    border-radius: 10px;
-  }
-  .bsc-egt-block[data-type="tbt"]{
-    border-color: #991594 !important;
-  }
+  
 }
 </style>
