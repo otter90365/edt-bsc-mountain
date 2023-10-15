00ba4b621ba71883aa7f5f4649abd1a834f9ee2d
starbit_phoebe <phoebe.cheng@star-bit.io>
2023-10-15 22:38:02 +0800

    [add] title-block component

---
+++ src/components/titleBlock.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="title-block d-flex flex-column align-center text-center font-weight-bold">
+    <h1 class="title-text d-inline-block" :class="titleFontSize">
+      {{ $t(title) }}
+    </h1>
+    <div class="subtitle-text mt-n1">{{ $t(subtitle) }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    titleFontSize: {
+      type: String,
+      default: 'rem-12'
+    },
+    subtitle: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.title-block {
+  .title-text {
+    padding: 0 60px;
+    background: url(../assets/img/deco-title.svg) center bottom / contain no-repeat;
+  }
+  .subtitle-text {
+    color: #6D6F79;
+  }
+}
+</style>
