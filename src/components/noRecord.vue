4e4786cde092ed70c99d206c54058fa4b895c256
starbit_phoebe <phoebe.cheng@star-bit.io>
2023-10-16 02:48:14 +0800

    [update] component (no-record) 改版

--- src/components/noRecord.vue
+++ src/components/noRecord.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="no-record-block d-flex justify-center align-center rounded-lg">
+  <div class="no-record-block d-flex justify-center align-center white--text font-weight-bold">
     {{ $t('noRecord') }}
   </div>
 </template>
@@ -12,7 +12,7 @@ export default {
 
 <style scoped>
 .no-record-block{
-  background-color: #EDFEFE;
+  background-color: #D3D3D3;
   height: 200px;
 }
 </style>
