7b66657b4c11d20f517bb568b3f16c4fc7f8dd07
starbit_phoebe <phoebe.cheng@star-bit.io>
2023-10-16 00:23:18 +0800

    [update] component (warning) 改版

--- src/components/warning.vue
+++ src/components/warning.vue
@@ -5,28 +5,28 @@
       <h2 class="red--text mb-5">{{ $t('warning') }}</h2>
       <div class="font-weight-bold">
         <p>{{ $t('warningRegistry') }}</p>
-        <h3 class="text-decoration-underline can-click text-center mb-3" :class="`primary_${$route.params.token}--text`" @click="$router.push({name: 'Defi-registry'})">{{ $t('toRegistry') }}</h3>
+        <h3 class="text-decoration-underline can-click text-center mb-3" :class="`primary--text`" @click="$router.push({name: 'Defi-registry'})">{{ $t('toRegistry') }}</h3>
         <p>{{page==='borrow' ? $t('warningBorrow') : $t('warningDeposit')}}</p>
         <p>{{ $t('warningCalculate') }}</p>
       </div>
-      <h3 class="text-decoration-underline can-click" :class="`primary_${$route.params.token}--text`" @click="$router.push({name: 'Calculator'})">{{ $t('toCalculator') }}</h3>
-      <v-checkbox v-model="isConfirm" :color="`primary_${$route.params.token}`">
+      <h3 class="text-decoration-underline can-click" :class="`primary--text`" @click="$router.push({name: 'Calculator'})">{{ $t('toCalculator') }}</h3>
+      <v-checkbox v-model="isConfirm" :color="`primary`">
         <template v-slot:label>
-          <div :class="`primary_${$route.params.token}--text`">{{ $t('agree') }}</div>
+          <div :class="`primary--text`">{{ $t('agree') }}</div>
         </template>
       </v-checkbox>
-      <btn :buttonText="'confirm'" :color="`primary_${$route.params.token}`" :width="270" @clickBtn="confirm()"></btn>
+      <btn :buttonText="'confirm'" :color="`primary`" :width="270" @clickBtn="confirm()"></btn>
     </v-card>
 
     <!--彩金池規則-->
     <v-card v-else id="rule" class="d-flex flex-column justify-center align-center pa-md-10 pa-5">
-      <h3 class="mb-5" :class="`primary_${$route.params.token}--text`">{{ content }}</h3>
+      <h3 class="mb-5" :class="`primary--text`">{{ content }}</h3>
       <div class="font-weight-bold">
         <ol class="mb-5 text-left">
           <li v-for="(text, i) in rules" :key="i" style="white-space: pre-wrap;" v-text="text"></li>
         </ol>
       </div>
-      <btn :buttonText="'確認'" :width="270" :color="`primary_${$route.params.token}`" @clickBtn="closeRule()"></btn>
+      <btn :buttonText="'確認'" :width="270" :color="`primary`" @clickBtn="closeRule()"></btn>
     </v-card>
   </v-dialog>
 </template>
