cb7e3840c5c601ef0cba799f08363a40d37a26d6
starbit_phoebe <phoebe.cheng@star-bit.io>
2023-10-23 03:00:49 +0800

    [update] EGT 換算器 USDT (calculator-usdt) 改版

--- src/views/Calculator/usdt.vue
+++ src/views/Calculator/usdt.vue
@@ -1,45 +1,51 @@
 <template>
   <div class="calculator-usdt-page">
     <v-row justify="center" class="my-15">
-      <v-col cols="11" sm="9" md="6">
-        <v-card light class="calculator-card card-wrap d-flex flex-column justify-center align-center">
-          <img src="@/assets/img/icon-usdt.png" width="60px" class="mb-5">
-          <h2 :class="`primary_${$route.params.token}--text`">{{ $t('vsEn') }} EGT {{ $t('vs') }} USDT {{ $t('vsBack') }}</h2>
-          <div class="mb-10" :class="`primary_${$route.params.token}--text`">{{ $t('egtExchangeRate', {value: '1 : 1200'}) }}</div>
-          <v-row class="mb-10">
-            <v-col cols="12">
-              <v-form ref="form" lazy-validation class="d-flex flex-column justify-center align-center">
-                <v-text-field
-                  v-model="egtAmount"
-                  style="width: 250px;"
-                  outlined
-                  dense
-                  full-width
-                  :color="`primary_${$route.params.token}`"
-                  placeholder="0"
-                  label="EGT"
-                  :rules="[...TokenAmountRules]"
-                >
-                </v-text-field>
-                <v-icon class="mb-5">mdi-arrow-up-down-bold</v-icon>
-                <div class="d-flex flex-column align-center">
-                  <div>USDT {{ $t('amount') }}</div>
-                  <h2 :class="`primary_${$route.params.token}--text`">{{ usdtResult || '--'}} {{ $t('piece') }} USDT</h2>
-                </div>
-              </v-form>
-            </v-col>
-          </v-row>
-          <div v-if="updateTime" class="mb-3 red--text text-caption text-right" style="width: 100%;">{{ $t('dataUpdated') }} {{ timestampToTime(updateTime) }}</div>
-          <btn class="mb-5" :buttonText="'calculate'" :color="`primary_${$route.params.token}`" :isCenter="true" :width="270" @clickBtn="calculate()"></btn>
-          <div class="can-click" @click="$router.push({name: 'Home'})">{{ $t('backToIndex') }}</div>
-        </v-card>
+      <v-col cols="11" sm="9">
+        <borderWrapCard class="mb-13 calculator-card">
+          <template #content>
+            <titleBlock class="mb-8" :title="$t('swapCalculateTitle', {token1: 'EGT', token2: 'USDT'})" :subtitle="$t('egtExchangeRate', {value: '1 : 1200'})" icon="icon-usdt-black.svg"></titleBlock>
+
+            <v-row class="mb-10">
+              <v-col cols="12">
+                <v-form ref="form" lazy-validation class="d-flex flex-column justify-center align-center">
+                  <v-text-field
+                    v-model="egtAmount"
+                    style="width: 250px;"
+                    class="rounded-0"
+                    outlined
+                    dense
+                    full-width
+                    color="secondary"
+                    placeholder="0"
+                    label="EGT"
+                    :rules="[...TokenAmountRules]"
+                  >
+                  </v-text-field>
+                  <v-icon class="mb-n3" color="secondary">mdi-chevron-up</v-icon>
+                  <v-icon class="mb-5" color="secondary">mdi-chevron-down</v-icon>
+                  <div class="d-flex flex-column align-center">
+                    <div>USDT {{ $t('amount') }}</div>
+                    <h2 class="secondary--text">{{ usdtResult || '--'}} {{ $t('piece') }} USDT</h2>
+                  </div>
+                </v-form>
+              </v-col>
+            </v-row>
+
+            <div v-if="updateTime" class="mb-3 red--text text-caption text-right w-100">{{ $t('dataUpdated') }} {{ timestampToTime(updateTime) }}</div>
+            <imgBtn class="mb-5" dark type="bg-black-sloped" buttonText="calculate" @clickBtn="calculate()"></imgBtn>
+            <imgBtn class="mb-3" type="border-black-sloped" buttonText="backToIndex" @clickBtn="$router.push({name: 'Home'})"></imgBtn>
+          </template>
+        </borderWrapCard>
       </v-col>
     </v-row>
   </div>
 </template>
 <script>
 import base from '@/mixin/base.js'
-import btn from '@/components/btn.vue'
+import borderWrapCard from '@/components/borderWrapCard.vue'
+import titleBlock from '@/components/titleBlock.vue'
+import imgBtn from '@/components/imgBtn.vue'
 export default {
   name: 'Calculator-usdt',
   mixins: [base],
@@ -51,7 +57,9 @@ export default {
     }
   },
   components:{
-    btn
+    borderWrapCard,
+    titleBlock,
+    imgBtn
   },
   methods:{
     async calculate(){
